a92009802049a51b15f9c5152cf567d74ee59ed0
ianwild <imw@acm.org>
2015-10-29 21:29:01 +0100

    added integer comparison functions

--- src/integer.h
+++ src/integer.h
@@ -3,9 +3,16 @@
 
 #include "types.h"
 
-obj fn_plus   (obj args);
-obj fn_times  (obj args);
-obj fn_minus  (obj args);
-obj fn_divide (obj args);
+obj fn_plus         (obj args);
+obj fn_times        (obj args);
+obj fn_minus        (obj args);
+obj fn_divide       (obj args);
+
+obj fn_lt           (obj args);
+obj fn_le           (obj args);
+obj fn_gt           (obj args);
+obj fn_ge           (obj args);
+obj fn_equals       (obj args);
+obj fn_not_equals   (obj args);
 
 #endif /* QSL_INTEGER_H */
