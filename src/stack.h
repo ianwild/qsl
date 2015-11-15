58548da1fd649686306eb081ea09b4f7512920ab
ianwild <imw@acm.org>
2015-11-15 22:30:29 +0100

    Started some notes about the bytecode strategy

---
+++ src/stack.h
@@ -0,0 +1,12 @@
+#ifndef QSL_STACK_H
+#define QSL_STACK_H
+
+/*
+  Instead of passing a newly-consed array on each function call,
+  the bytecode version will have an implicit stack, and simply
+  pass the number of arguments the called function should consume.
+
+  These are the accessor functions for the stack.
+*/
+
+#endif /* QSL_STACK_H */
