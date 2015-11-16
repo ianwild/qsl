14af301f28cf2617618114cbca598e23b7f8926d
ianwild <imw@acm.org>
2015-11-16 21:38:58 +0100

    More notes

--- src/eval.h
+++ src/eval.h
@@ -1,6 +1,27 @@
 #ifndef QSL_EVAL_H
 #define QSL_EVAL_H
 
+/*
+
+  If eval sees (while (< n 10) (setq n (+ n 1)))
+    - since 'while' isn't a true function, 'eval' calls a special handler
+    - 'while' then needs to call 'eval' for each clause in the body
+    - this nested 'eval' needs a special handler for the 'setq'
+    - 'setq' calls *yet another* 'eval' to do the +
+
+  This eval-recursion happens at both the C and Lisp levels.  If we
+  were a bit smarter, though, we could flatten the original expression
+  to something like:
+    XXX: n@, 10, <, jump-if-nil YYY, n@, 1, +, n!, jump XXX, YYY:
+  which can be evaluated with *no* recursion.  Yes, we still need the
+  original ping-pong to do this compilation, but only at the C level.
+
+  Problem: what happens to any nested (lambda ...) expressions during
+  compilation?
+
+*/
+  
+
 #include "target.h"
 #include "types.h"
 
