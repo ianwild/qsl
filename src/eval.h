e0d5c98c99a2687df6af790f2cb55edf115272b2
ianwild <imw@acm.org>
2015-11-18 20:39:27 +0100

    More notes

--- src/eval.h
+++ src/eval.h
@@ -19,6 +19,15 @@
   Problem: what happens to any nested (lambda ...) expressions during
   compilation?
 
+  How about ... An inner "compiled lambda" is only a skeleton - it
+  needs to be combined with an environment before it becomes runnable.
+  So, we can create a sort of "bookmark", a "compiled lambda" object
+  whose "body" field is the (nested) list to be compiled and whose
+  "environment" field is a marker saying "not yet compiled".  Then,
+  once the outer clause has been compiled, we go back and look for
+  these bookmarks.  That way we can compile arbitrarily nested lambdas
+  without recursing in the compiler.
+
 */
   
 
